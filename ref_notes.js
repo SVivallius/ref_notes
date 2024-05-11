a3987426e99ebeeef90d010bb564413540de0b30
Stefan Vivallius <varulvsnatt@hotmail.com>
2024-05-11 13:25:09 +0200

    Init commit

---
+++ ref_notes.js
@@ -0,0 +1,146 @@
+function(context, args)
+{
+	return (
+		{
+			"Companies": [
+				"nuutec",
+				"setec_gas",
+				"amal_robo",
+				"aon",
+				"archaic",
+				"bluebun",
+				"bunnybat_hut",
+				"context",
+				"core",
+				"cyberdine",
+				"empty_nest",
+				"etceteraco"
+			],
+			"Passwords": [
+				"endtheworld"
+			],
+			"Employees" :{
+				"Mitch": {
+					"Sector": "HR"
+				},
+				"Lynne": {
+					"Sector": "IT"
+				},
+				"Paolo":{
+					"Sector": "Marketing"
+				},
+				"Sherlise": {
+					"Sector": "DEVOPS"
+				},
+				"Jimmy": {
+					"Sector": "DEVOPS"
+				},
+				"Sergio":{
+					"Sector": "BIZDEV"
+				}
+			},
+			"Usernames": [
+				"rocky_b",
+				"d0ctor_wh0m",
+				"duke_ell",
+				"bassy_thecount",
+				"thegreat",
+				"lass_doug",
+				"jamesb",
+				"theshrillery",
+				"purple1",
+				"ada_love",
+				"sammy_l_jack",
+				"daurmith",
+				"indie_jones",
+				"diamond_dogz",
+				"_3rd_3y3_grill",
+				"derek_zoo",
+				"d4ria",
+				"rain3y",
+				"corg_train",
+				"free_man_morg",
+				"du_boyz",
+				"be_lavar",
+				"king_in_yellow",
+				"poitier_27"
+			],
+			"Unknowns": [
+				"csrbot",
+				"knowledgebase",
+				"kb_bot",
+				"turn_a_nat",
+				"custbot",
+				"clarence"
+			],
+			"Projects": [
+				"H0meEntert4inment",
+				"BL4Z1NGW0RLD",
+				"Free_BFG",
+				"qw_stop",
+				"101010",
+				"giant_spidr",
+				"omegabyte_03.23_final_final",
+				"thefloood",
+				"W3rlD3NDER",
+				"judgeme_nt",
+				"Vy_for_russ",
+				"Ap_calypse",
+				"forgetme_nt",
+				"tmp",
+				"semordnilap.sh",
+				"cpanel",
+				"ragnaroxx.sh",
+				"delete_me_first",
+				"cloudskimmer.da7a",
+				"ls_rites",
+				"ende.exe",
+				"dev_nul",
+				"e)(ecution0r",
+				"ESCHATOLOGI.EXE",
+				"LUNARLANDER_01.11.bat"
+			],
+			"Pages": [
+				"userpanel",
+				"personnel",
+				"empl_pages",
+				"profiles",
+				"p33ps",
+				"__av93zk9b9",
+				"public_profiles",
+				"myaccount"
+			],
+			"Nav-Commands":[
+				"navigation",
+				"open",
+				"nav",
+				"command",
+				"action"
+			],
+			"Clues":[
+				"v__to __'s tavern. bring the stuff",
+				"CLI.public"
+			],
+			"acct_nt_clues": [
+				"Get me the amount of a large withdrawal near 240511.0714", //large withdrawal
+				"What was the net GC between 240511.0646 and 240511.0714" //net GC
+			],
+			"T2-Locks":{
+				"CON_SPEC": "Provide a scriptor <number>({s:<string>, d:<number>}) which returns the number of times the digit shows up in the string",
+				"acct_nt": "Calculate net GC between timestamps, sometimes excluding with memos, sometimes exclusive with memos"
+			},
+			"NPC-fragments":{
+				"prefixes":[
+					"abndnd",
+					"abandoned",
+					"anon",
+					"anonymous",
+					"derelict",
+					"uknown",
+					"unknown",
+					"unidentified"
+				]
+			}
+		}
+	)
+}
